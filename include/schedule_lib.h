ed74d1b8a93d0c44713c91601f8ccae726501caf
fpekal <proxy154d@gmail.com>
2022-07-30 20:59:22 +0200

    Legacy code

---
+++ include/schedule_lib.h
@@ -0,0 +1,84 @@
+#ifndef SCHEDULE_LIB_H
+#define SCHEDULE_LIB_H
+
+#include <thread>
+#include <atomic>
+#include <functional>
+#include <chrono>
+#include <list>
+
+class Schedule {
+public:
+	static Schedule instance;
+
+private:
+	Schedule() : thread{ std::bind(&Schedule::thread_func, this) }, active{ true }, sync{ false } {}
+
+	~Schedule();
+
+	std::thread thread;
+	bool active;
+
+	void thread_func();
+
+	void synchronize();
+
+	void iterate_delays();
+	void iterate_repeats();
+
+public:
+	class Delay {
+		friend class Schedule;
+
+		std::chrono::seconds duration;
+		std::function<void()> function;
+
+		std::chrono::system_clock::time_point start_point;
+
+		Delay() : start_point{ std::chrono::system_clock::now() }, duration{ 0 }, function{ nullptr } {};
+
+	public:
+		// Set delay after which callable object will be called.
+		Delay& time(std::chrono::seconds duration);
+		// Set callable object to call.
+		Delay& call(std::function<void()> function);
+	};
+
+	// Create `Delay` object.
+	static Delay& make_delay();
+
+	class Repeating {
+		friend class Schedule;
+
+		std::chrono::seconds interval;
+		std::function<void()> function;
+
+		std::chrono::system_clock::time_point last_call;
+
+		std::list<Repeating>::iterator iterator;
+
+		Repeating() : last_call{ std::chrono::system_clock::now() }, interval{ 0 }, function{ nullptr } {};
+
+	public:
+		// Set interval for calling.
+		Repeating& time(std::chrono::seconds interval);
+		// Set callable object to call.
+		Repeating& call(std::function<void()> function);
+		// Stop further repeating and delete object.
+		void stop();
+	};
+
+	// Create `Repeating` object.
+	static Repeating& make_repeating();
+
+private:
+	std::list<Delay> delays;
+	std::list<Repeating> repeats;
+
+	std::atomic<bool> sync;		// idk if it's needed //TODO: test that
+
+	friend class Schedule::Delay;
+	friend class Schedule::Repeating;
+};
+
+#endif
---
+++ src/schedule_lib.cpp
@@ -0,0 +1,87 @@
+#include "../include/schedule_lib.h"
+
+static Schedule Schedule::instance;
+
+Schedule::~Schedule() {
+	active = false;
+	thread.join();
+}
+
+void Schedule::thread_func() {
+	while(active) {
+		synchronize();
+		iterate_delays();
+		iterate_repeats();
+	}
+}
+
+void Schedule::synchronize() {
+	if (sync.load(std::memory_order_acquire)) {
+		sync.store(false, std::memory_order_relaxed);
+	}
+}
+
+void Schedule::iterate_delays() {
+	for (auto iter = delays.begin(); iter != delays.end(); ++iter) {
+		auto delta_time = std::chrono::system_clock::now() - iter->start_point;
+
+		if (delta_time >= iter->duration) {
+			if (iter->function != nullptr)
+				iter->function();
+			delays.erase(iter);
+			break;
+		}
+	}
+}
+
+void Schedule::iterate_repeats() {
+	for (auto iter = repeats.begin(); iter != repeats.end(); ++iter) {
+		auto delta_time = std::chrono::system_clock::now() - iter->last_call;
+
+		if (delta_time >= iter->interval) {
+			if (iter->function != nullptr)
+				iter->function();
+			iter->last_call = std::chrono::system_clock::now();
+		}
+	}
+}
+
+Schedule::Delay& Schedule::Delay::time(std::chrono::seconds duration) {
+	this->duration = duration;
+	instance.sync.store(true, std::memory_order_release);
+	return *this;
+}
+
+Schedule::Delay& Schedule::Delay::call(std::function<void()> function) {
+	this->function = function;
+	instance.sync.store(true, std::memory_order_release);
+	return *this;
+}
+
+Schedule::Delay& Schedule::make_delay() {
+	instance.delays.push_back(Delay{});
+	instance.sync.store(true, std::memory_order_release);
+	return instance.delays.back();
+}
+
+Schedule::Repeating& Schedule::Repeating::time(std::chrono::seconds interval) {
+	this->interval = interval;
+	instance.sync.store(true, std::memory_order_release);
+	return *this;
+}
+
+Schedule::Repeating& Schedule::Repeating::call(std::function<void()> function) {
+	this->function = function;
+	instance.sync.store(true, std::memory_order_release);
+	return *this;
+}
+
+void Schedule::Repeating::stop() {
+	instance.repeats.erase(iterator);
+}
+
+Schedule::Repeating& Schedule::make_repeating() {
+	instance.repeats.push_back(Repeating{});
+	instance.sync.store(true, std::memory_order_release);
+	return instance.repeats.back();
+}
